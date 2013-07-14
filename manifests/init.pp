8b67dcf242589a1a644600ebaa16a55fe09f1492
poppen <poppen.jp@gmail.com>
2013-07-15 07:25:19 +0900

    change to install the version for Mountain Lion

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,7 +7,7 @@
 class sophos_antivirus
 {
   package { 'sophos_antivirus':
-    source   => 'http://downloads.sophos.com/home-edition/savosx80_he.dmg',
+    source   => 'http://downloads.sophos.com/home-edition/savosx_80_he.10.8+.dmg',
     provider => 'pkgdmg',
   }
 }
--- spec/classes/sophos_antivirus_spec.rb
+++ spec/classes/sophos_antivirus_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'sophos_antivirus' do
   it do
     should contain_package('sophos_antivirus').with({
-      :source => 'http://downloads.sophos.com/home-edition/savosx80_he.dmg',
+      :source => 'http://downloads.sophos.com/home-edition/savosx_80_he.10.8+.dmg',
       :provider => 'pkgdmg',
     })
   end
